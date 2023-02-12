edca2fa4c1d3d4d273b89fb6a7ad2f7b04121976
Charles Chen <chen.charles.c@gmail.com>
2023-02-12 13:07:07 -0500

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,26 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: 'https://chrlschn.dev',
+  integrations: [mdx(), sitemap(), tailwind(), prefetch()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'monokai',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: []
+    }
+  }
 });
